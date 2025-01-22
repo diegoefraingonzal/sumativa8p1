88ad78cdcf6f96f0487b872c5539ea1e02048930
diegoefraingonzal <13064@portalcedi.edu.mx>
2025-01-21 22:59:26 -0600

    Commit Producto Final

--- ea8/src/app/home/home.page.ts
+++ ea8/src/app/home/home.page.ts
@@ -1,26 +1,25 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  imports: [IonicModule, CommonModule],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
   userForm: FormGroup;
-  users: Array<{ name: string; email: string; age: number }> = [];
+  users: { name: string; email: string; age: number }[] = [];
   editingIndex: number | null = null;
 
-  constructor(private fb: FormBuilder) {
-    this.userForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(3)]],
-      email: ['', [Validators.required, Validators.email]],
-      age: ['', [Validators.required, Validators.min(18)]]
+  constructor() {
+    this.userForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      age: new FormControl('', [Validators.required, Validators.min(1)]),
     });
   }
 
@@ -37,17 +36,16 @@ export class HomePage {
     }
   }
 
-  deleteUser(index: number) {
-    this.users.splice(index, 1);
+  editUser(index: number) {
+    this.editingIndex = index;
+    this.userForm.setValue({
+      name: this.users[index].name,
+      email: this.users[index].email,
+      age: this.users[index].age,
+    });
   }
 
-  editUser(index: number) {
-    const user = this.users[index];
-    this.userForm.setValue({
-      name: user.name,
-      email: user.email,
-      age: user.age
-    });
-    this.editingIndex = index;
+  deleteUser(index: number) {
+    this.users.splice(index, 1);
   }
 }
